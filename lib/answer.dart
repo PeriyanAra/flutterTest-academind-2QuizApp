0fd7154f65bdef4640adc22046b12e123c568178
Ara <periyan.ara@gmail.com>
2020-02-23 22:13:45 +0400

    list to wodgets

---
+++ lib/answer.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+
+class Answer extends StatelessWidget {
+  final Function selectHandler;
+  final String answerText;
+
+  const Answer({
+    Key key,
+    this.selectHandler,
+    this.answerText
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      child: RaisedButton(
+        color: Colors.blue,
+        textColor: Colors.white,
+        child: Text(answerText),
+        onPressed: selectHandler,
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,56 @@
 import 'package:flutter/material.dart';
 
+import 'question.dart';
+import 'answer.dart';
+
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
-  void answerQuestion() {
-    print('Answer chosen!');
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  var _questionIndex = 0;
+
+  void _answerQuestion() {
+    setState(() {
+      _questionIndex = _questionIndex + 1;
+    });
+    
+    print(_questionIndex);
   }
 
   @override
   Widget build(BuildContext context) {
     var questions = [
-      'What\'s you favorite color?',
-      'What\'s you favorite animal?'
+      {
+        'questionText': 'What\'s your favorite color?',
+        'answers': [
+          'Black',
+          'Red',
+          'Green',
+          'White'
+        ],
+      },
+      {
+        'questionText': 'What\'s your favorite animal?',
+        'answers': [
+          'Rabbit',
+          'Snake',
+          'Elephant',
+          'Lion'
+        ],
+      },
+      {
+        'questionText': 'Who\'s your favorite instructor?',
+        'answers': [
+          'Max',
+          'John',
+          'Bob',
+          'Tom'
+        ],
+      },
     ];
     
     return MaterialApp(
@@ -21,19 +60,15 @@ class MyApp extends StatelessWidget {
         ),
         body: Column(
           children: <Widget>[
-            Text('The question!'),
-            RaisedButton(
-              child: Text('Answer 1'),
-              onPressed: answerQuestion,
+            Question(
+              questionText: questions[_questionIndex]['questionText']
             ),
-            RaisedButton(
-              child: Text('Answer 2'),
-              onPressed: answerQuestion,
-            ),
-            RaisedButton(
-              child: Text('Answer 3'),
-              onPressed: answerQuestion,
-            ),
+            ...(questions[_questionIndex]['answers'] as List<String>).map((answer) {
+              return Answer(
+                answerText: answer,
+                selectHandler: _answerQuestion
+              );
+            }).toList()
           ],
         ),
       ),
